c01001c21fee6cd270f106edb80af024da608ea1
VictorBTch <victorborgesben10@gmail.com>
2025-10-14 15:13:05 -0300

    Add files via upload

---
+++ atv 23.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+	int numero;		
+	
+printf("Digite um numero qualquer: \n");	
+	scanf("%d", &numero);
+	
+if(numero % 2 == 0)	
+{
+printf("numero par \n");
+return 0;
+}	
+	else
+	{	
+	printf("numero impar \n");
+	return 0;
+	}
+}
